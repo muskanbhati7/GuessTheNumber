5b732e096c2ef263e94cb396aed4ce4b6d053fdd
muskanbhati7 <68704151+muskanbhati7@users.noreply.github.com>
2021-01-17 21:48:24 +0530

    Update Ex3_GuessTheNumber.java

--- Ex3_GuessTheNumber.java
+++ Ex3_GuessTheNumber.java
@@ -42,15 +42,7 @@ class Game{
 }
 public class Ex3_GuessTheNumber {
     public static void main(String[] args) {
-        /*
-            Create a class Game, which allows a user to play "Guess the Number"
-            game once. Game should have the following methods:
-            1. Constructor to generate the random number
-            2. takeUserInput() to take a user input of number
-            3. isCorrectNumber() to detect whether the number entered by the user is true
-            4. getter and setter for noOfGuesses
-            Use properties such as noOfGuesses(int), etc to get this task done!
-         */
+      
 
         Game g = new Game();
         boolean b = false;
